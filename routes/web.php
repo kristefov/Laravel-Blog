fea54bd431e2b50865e7d91c05bc4a9302278193
kristefov <kristiyan.tefov@icloud.com>
2024-08-08 18:12:41 +0100

    adding functionality so only admin can crud the categories

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('categories', \App\Http\Controllers\CategoryController::class)->middleware(\App\Http\Middleware\IsAdminMiddleware::class);
+
+
 });
 
-Route::resource('categories', \App\Http\Controllers\CategoryController::class);
 
 require __DIR__.'/auth.php';
