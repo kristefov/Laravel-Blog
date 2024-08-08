23b855fe051b4f938fe451cd036c083765e9fe8c
kristefov <kristiyan.tefov@icloud.com>
2024-08-08 17:26:16 +0100

    adding crud operations for catogory model

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+
 require __DIR__.'/auth.php';
